950523e495bd7711fe9109d08ce5a86499c88a28
ElMooro <raafouis@gmail.com>
2025-05-06 00:52:34 +0000

    Refactor WatchlistComponent based on code review feedback

---
+++ src/components/WatchlistComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WatchlistComponent() {
+  return (
+    <div className="watchlist">
+      <p>Market watchlist would render here</p>
+    </div>
+  );
+}
+
+export default WatchlistComponent;
